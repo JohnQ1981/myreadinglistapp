58f0f376999d2b9900eeb37acd37336f957d472d
JohnQ1981 <ikram1981@gmail.com>
2023-01-22 18:49:10 -0600

    this version is done

--- myreadinglistappjq/src/components/BookList.js
+++ myreadinglistappjq/src/components/BookList.js
@@ -1,13 +1,21 @@
-import BookShow from './BookShow';
+import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/books";
 
-function BookList ({books, onDelete, onEdit}){
-    const renderedBooks= books.map((book)=>{
-        return <BookShow onDelete = {onDelete}  onEdit={onEdit} key ={book.id} book={book}/>;
+function BookList({ books, onDelete, onEdit }) {
+  const {count, incrementCount} = useContext(BooksContext);
 
-    });
-
+  const renderedBooks = books.map((book) => {
     return (
-        <div className="book-list">{renderedBooks}</div>
+      <BookShow onDelete={onDelete} onEdit={onEdit} key={book.id} book={book} />
     );
+  });
+
+  return (
+    <div className="book-list">
+      <h3>{count}<button onClick ={incrementCount}>Click Me</button></h3>
+      {renderedBooks}
+    </div>
+  );
 }
 export default BookList;
